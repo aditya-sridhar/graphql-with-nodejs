2001739c27a66a9c37f394c235ee675e5d9c23b3
Aditya S <s.aditya.1811@gmail.com>
2018-12-20 20:24:28 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,132 @@
+//get all the libraries needed
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLList
+} = require('graphql');
+const _ = require('lodash');
+
+//setting up the port number and express app
+const port = 5000;
+const app = express();
+
+
+//Hardcode some data for movies and directors
+let movies = [{
+    id: 1,
+    name: "Movie 1",
+    year: 2018,
+    directorId: 1
+},
+{
+    id: 2,
+    name: "Movie 2",
+    year: 2017,
+    directorId: 1
+},
+{
+    id: 3,
+    name: "Movie 3",
+    year: 2016,
+    directorId: 3
+}
+];
+
+let directors = [{
+    id: 1,
+    name: "Director 1",
+    age: 20
+},
+{
+    id: 2,
+    name: "Director 2",
+    age: 30
+},
+{
+    id: 3,
+    name: "Director 3",
+    age: 40
+}
+];
+
+// Define Movie Type
+const movieType = new GraphQLObjectType({
+    name: 'Movie',
+    fields: {
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        year: { type: GraphQLInt },
+        directorId: { type: GraphQLID }
+
+    }
+});
+
+//Define Director Type
+const directorType = new GraphQLObjectType({
+    name: 'Director',
+    fields: {
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        movies: {
+            type: new GraphQLList(movieType),
+            resolve(source, args) {
+                return _.filter(movies, { directorId: source.id });
+            }
+
+        }
+
+    }
+})
+
+//Define the Query
+const queryType = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        hello: {
+            type: GraphQLString,
+
+            resolve: function () {
+                return "Hello World";
+            }
+        },
+
+        movie: {
+            type: movieType,
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: function (source, args) {
+                return _.find(movies, { id: args.id });
+            }
+        }
+
+        ,
+
+        director: {
+            type: directorType,
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: function (source, args) {
+                return _.find(directors, { id: args.id });
+            }
+        }
+    }
+});
+
+// Define the Schema
+const schema = new GraphQLSchema({ query: queryType });
+
+//Setup the nodejs GraphQL server
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+}));
+
+app.listen(port);
+console.log(`GraphQL Server Running at localhost:${port}`);
